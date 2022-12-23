721d5a3b0479d527ce2b3f99859abb6bba19a4c5
Sharibo <AlexejKrawez@gmail.com>
2022-12-23 13:25:26 +0300

    add: task02

--- src/main/java/com/gmail/alexejkrawez/Music.java
+++
@@ -1,5 +0,0 @@
-package com.gmail.alexejkrawez;
-
-public interface Music {
-    String getSong();
-}
--- src/main/java/com/gmail/alexejkrawez/RockMusic.java
+++
@@ -1,8 +0,0 @@
-package com.gmail.alexejkrawez;
-
-public class RockMusic implements Music {
-    @Override
-    public String getSong() {
-        return "Wind cries Mary";
-    }
-}
